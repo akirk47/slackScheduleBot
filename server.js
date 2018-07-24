1651f5464721602fa51db6f5236098f691dd0dad
akirk47 <andrewkirk093@gmail.com>
2018-07-24 15:05:17 -0700

    added some heroku shit

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const app = express()
 var path = require('path');
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
   res.render('login')
 })
+
+app.post('/hello', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello ' + userName + ', welcome to Devdactic Slack channel! I\'ll be your guide.'
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
