2551f9cddef33179f6300f153ad3c0fb3534330d
akirk47 <andrewkirk093@gmail.com>
2018-07-24 14:08:14 -0700

    added login hbs

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+var path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('login')
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
